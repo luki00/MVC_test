bbe95a958ae139831449566557e7f5c1b87624bb
LaiQ <laiqus@windowslive.com>
2019-06-03 14:18:21 +0200

    Zmiana strony kontaktowej.

--- MVC_test/Controllers/HomeController.cs
+++ MVC_test/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MVC_test.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Opis.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Dane kontaktowe:";
 
             return View();
         }
